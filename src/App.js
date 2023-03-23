a5696ee51079f904f9962711c5d7f52d61399c32
Niranjan Kumar <niranjankumaraina@gmail.com>
2023-03-23 08:26:41 +0530

    23-03-2023

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
-import './App.css';
-import Carousel from './components/Carousel';
-import Navbar from './components/Navbar'
-import Cards from './components/Cards'
-import Footer from './components/footer'
-import About  from './components/About';
-import Login from './components/Login'
-import Signup from './components/Signup'
-import Contact from './components/Contact';
-import TaskManager from './components/TaskManager';
+import "./App.css";
+import Carousel from "./components/Carousel";
+import Navbar from "./components/Navbar";
+import Cards from "./components/Cards";
+import Footer from "./components/footer";
+import About from "./components/About";
+import Login from "./components/Login";
+import Signup from "./components/Signup";
+import Contact from "./components/Contact";
+import TaskManager from "./components/TaskManager";
+
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
 function App() {
+
   return (
-   <>
-   <Navbar mode = "dark"/>
-   {/* <Carousel/>
-   <Cards/> */}
-   {/* <Login /> */}
-   {/* <Signup /> */}
-   {/* <Contact /> */}
-   <About/>
-   {/* <TaskManager /> */}
-   <Footer/>
-   </>
+    <Router>
+      <>
+        <Navbar mode="dark" login = "login" />
+        <Switch>
+          <Route exact path="/">
+            <Carousel />
+            <Cards />
+          </Route>
+          <Route exact path="/login">
+            <Login />
+          </Route>
+          <Route exact path="/signup">
+            <Signup />
+          </Route>
+          <Route exact path="/contact">
+            <Contact />
+          </Route>
+          <Route exact path="/taskManager">
+            <TaskManager />
+          </Route>
+          <Route exact path="/about">
+            <About />
+          </Route>
+        </Switch>
+        <Footer />
+      </>
+    </Router>
   );
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,59 +1,69 @@
 import React from "react";
+import {BrowserRouter as Router, Link } from "react-router-dom";
 
 function navbar(props) {
   return (
-    <div>
-      <nav
-        className={`navbar navbar-expand-lg bg-${props.mode} navbar-${props.mode} `}
-      >
-        <div className="container-fluid">
-          <a className="navbar-brand" href="/">
-            Todo
-          </a>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-              <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="/">
-                  Home
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="/">
-                  Task Manager
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="/">
-                  Contact
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="/">
-                  About
-                </a>
-              </li>
-            </ul>
-            <button type="button " className="btn btn-danger nav-item mx-2">
-              Login
+    <Router>
+      <div>
+        <nav
+          className={`navbar navbar-expand-lg bg-${props.mode} navbar-${props.mode} `}
+        >
+          <div className="container-fluid">
+            <Link to="/" className="navbar-brand">
+              Todo
+            </Link>
+            <button
+              className="navbar-toggler"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#navbarSupportedContent"
+              aria-controls="navbarSupportedContent"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
+              <span className="navbar-toggler-icon"></span>
             </button>
-            <button type="button " className="btn btn-danger nav-item mx-2">
-              Signup
-            </button>
+            <div
+              className="collapse navbar-collapse"
+              id="navbarSupportedContent"
+            >
+              <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                <li className="nav-item">
+                  <Link to="/" className="nav-link active" aria-current="page">
+                    Home
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link to="/taskManager" className="nav-link">
+                    Task Manager
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link to="/contact" className="nav-link">
+                    Contact
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link to="/about" className="nav-link">
+                    About
+                  </Link>
+                </li>
+              </ul>
+              <Link to={props.login} className="nav-link">
+                <button type="button " className="btn btn-danger nav-item mx-2">
+                  Login
+                </button>
+              </Link>
+              <Link to="/signup" className="nav-link">
+                <button type="button " className="btn btn-danger nav-item mx-2">
+                  Signup
+                </button>
+              </Link>
+            </div>
           </div>
-        </div>
-      </nav>
-    </div>
+        </nav>
+      </div>
+    </Router>
   );
 }
 
