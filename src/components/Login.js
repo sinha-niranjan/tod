fc1c46d35bf8b179c554878a01b8754bf1fdf906
Niranjan Kumar <niranjankumaraina@gmail.com>
2023-04-01 18:38:47 +0530

    01-04-2023

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,53 +1,81 @@
-import React from 'react'
-
+import React, { useState } from "react";
+let UseState = useState;
 function login() {
+  const [email, setEmail] = UseState("");
+  const [pass, setPass] = UseState("");
+  const [conPass, setConPass] = UseState("");
+  function getFormData(e) {
+    console.log(email, pass, conPass);
+
+    e.preventDefault();
+    if (pass === conPass) {
+      setEmail("");
+      setPass("");
+      setConPass("");
+    }
+    else{
+      alert("Please renter correct password !")
+    }
+  }
   return (
     <div>
       <main>
-        <div class="jumbotron jumbotron-fluid">
-          <div class="container">
-            <div class="row">
-              <div class="col-12 col-sm-8 col-md-6 col-lg-4 offset-sm-4 offset-sd-3 offset-lg-4">
-                <h1 class="mb-3 text-center">Todo log in</h1>
-                <form class="mb-3">
-                  <div class="form-group">
-                    <label for="email">Email:</label>
+        <div className="jumbotron jumbotron-fluid">
+          <div className="container">
+            <div className="row">
+              <div className="col-12 col-sm-8 col-md-6 col-lg-4 offset-sm-4 offset-sd-3 offset-lg-4">
+                <h1 className="mb-3 text-center">Todo log in</h1>
+                <form className="mb-3" onSubmit={getFormData}>
+                  <div className="form-group">
+                    <label htmlFor="email">Email:</label>
                     <input
                       type="email"
-                      class="form-control"
+                      className="form-control"
                       placeholder="example@email.com"
                       id="email"
+                      onChange={(e) => {
+                        setEmail(e.target.value);
+                      }}
+                      value={email}
                       required
                     />
                   </div>
-                  <div class="form-group">
-                    <label for="password">Enter Password:</label>
+                  <div className="form-group">
+                    <label htmlFor="password">Enter Password:</label>
                     <input
                       type="password"
-                      class="form-control"
+                      className="form-control"
                       id="password"
+                      onChange={(e) => {
+                        setPass(e.target.value);
+                      }}
+                      value={pass}
                       required
                     />
                   </div>
-                  <div class="form-group">
-                    <label for="password">Renter Password :</label>
+                  <div className="form-group">
+                    <label htmlFor="password">Renter Password :</label>
                     <input
                       type="password"
-                      class="form-control"
-                      id="password"
+                      className="form-control"
+                      id="confirm_password"
+                      onChange={(e) => {
+                        setConPass(e.target.value);
+                      }}
+                      value={conPass}
                       required
                     />
                   </div>
-                  <button type="submit" class="btn btn-primary btn-block">
+                  <button type="submit" className="btn btn-primary btn-block">
                     Login
                   </button>
                 </form>
-                <div class="text-center">
+                <div className="text-center">
                   <p>or..</p>
-                  <a href="create.html" class="btn btn-success">
+                  <a href="create.html" className="btn btn-success">
                     Create account
                   </a>
-                  <p class="small">
+                  <p className="small">
                     <a href="create.html">
                       Have you forgotten your account details?
                     </a>
@@ -57,10 +85,10 @@ function login() {
             </div>
           </div>
         </div>
-        <div class="form"></div>
+        <div className="form"></div>
       </main>
     </div>
   );
 }
 
-export default login
+export default login;
